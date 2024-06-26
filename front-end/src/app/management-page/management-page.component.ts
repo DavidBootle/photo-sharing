436c3aab67ac02be888f5beeefd77b2b1e572de0
David Bootle <davidtbootle@gmail.com>
2024-06-26 16:06:31 -0400

    Created basic management page

--- front-end/src/app/management-page/management-page.component.ts
+++ front-end/src/app/management-page/management-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TextBoxComponent } from '../text-box/text-box.component';
 
 @Component({
   selector: 'app-management-page',
   standalone: true,
-  imports: [],
+  imports: [TextBoxComponent],
   templateUrl: './management-page.component.html',
   styleUrl: './management-page.component.scss'
 })
