5505d1708b793cd232b04e98c48e0f7ec8e682b3
David Bootle <davidtbootle@gmail.com>
2024-06-26 15:23:55 -0400

    Added management page to routing

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component'
 import { UploadPageComponent } from './upload-page/upload-page.component';
+import { ManagementPageComponent } from './management-page/management-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent, pathMatch: 'full' }, // default route for home page
+    { path: 'manage', component: ManagementPageComponent },
     { path: 'upload', component: UploadPageComponent},
 ];
