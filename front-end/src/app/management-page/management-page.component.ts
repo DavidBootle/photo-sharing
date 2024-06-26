2e23d4e901b8f55a019e65ef8d8bfc319defebed
David Bootle <davidtbootle@gmail.com>
2024-06-26 15:34:23 -0400

    Revert "Moved management component code inline"
    This reverts commit fbf4aa14b6705f98665fcd599c106b6839f59a3f.

--- front-end/src/app/management-page/management-page.component.ts
+++ front-end/src/app/management-page/management-page.component.ts
@@ -4,24 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-management-page',
   standalone: true,
   imports: [],
-  template: `
-    <div class="full-page-container">
-        <p>I'm in the middle!</p>
-    </div>
-  `,
-  styles: `
-    .full-page-container {
-      width: 100vw;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
-
-    p {
-        color: white;
-    }
-  `,
+  templateUrl: './management-page.component.html',
+  styleUrl: './management-page.component.scss'
 })
 export class ManagementPageComponent {
 
